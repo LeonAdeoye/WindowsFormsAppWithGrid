7edd21a7a2a804b5b6228292f66c4ec36d04fd38
LeonAdeoye <leon.adeoye@gmail.com>
2022-05-11 21:22:57 +0800

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsAppWithGrid
+{
+    public class OrderInfoCollection
+    {
+
+    }
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //sfDataGrid1.DataSource = new OrderInfoCollection();
+        }
+
+        private void sfDataGrid1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
